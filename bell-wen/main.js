bbab100920341c2459a192ea2c397dfcf3db6085
wen1now <wen00@live.com.au>
2018-07-31 18:09:34 +1000

    made stuff better

--- bell-wen/main.js
+++ bell-wen/main.js
@@ -1,5 +1,6 @@
 function outputstring(){
     var x=new Date();
+    x.setTime(x.getTime()-v*500);
     if ((x.getHours()>=16)||((x.getHours()>=15)&&(x.getMinutes()>=10))){
         return ["School has finished","Finished"]
     }
@@ -8,31 +9,13 @@ function outputstring(){
         if (y==0){t="Sun"}else{t="sat"}
         return ["No school today",t]
     }
-    if ((y==1)||(y==5)){
-        z=["8:25","8:30","8:42","8:46","9:34","9:38","10:26","10:51","10:55","11:43","11:47","12:35","13:15","13:19","14:07","14:11","14:59","15:03","15:10"]
-        for (var i in z){
-            k=new Date(z[i]+" "+parseInt(x.getMonth()+1)+"/"+x.getDate()+"/"+x.getFullYear());
-            if (x.getTime()<k.getTime()){
-                return [format(k.getTime()-x.getTime()),format(k.getTime()-x.getTime())]
-            }
-        }
-    }
-    if ((y==2)||(y==3)){
-        z=["8:25","8:30","8:42","8:46","9:27","9:31","10:12","10:37","10:41","11:22","11:26","12:07","12:11","12:51","13:31","13:35","14:15","14:19","14:59","15:03","15:10"]
-        for (var i in z){
-            k=new Date(z[i]+" "+parseInt(x.getMonth()+1)+"/"+x.getDate()+"/"+x.getFullYear());
-            if (x.getTime()<k.getTime()){
-                return [format(k.getTime()-x.getTime()),format(k.getTime()-x.getTime())]
-            }
-        }
-    }
-    if (y==4){
-        z=["8:25","8:30","8:42","8:46","9:30","9:34","10:18","10:43","10:47","11:30","11:34","12:17","12:21","12:51","13:31","13:35","14:15","14:19","14:59","15:03","15:10"]
-        for (var i in z){
-            k=new Date(z[i]+" "+parseInt(x.getMonth()+1)+"/"+x.getDate()+"/"+x.getFullYear());
-            if (x.getTime()<k.getTime()){
-                return [format(k.getTime()-x.getTime()),format(k.getTime()-x.getTime())]
-            }
+    if ((y==1)||(y==5)){z=["8:25","8:30","8:42","8:46","9:34","9:38","10:26","10:51","10:55","11:43","11:47","12:35","12:55","13:15","13:19","14:07","14:11","14:59","15:03","15:10"]}
+    if ((y==2)||(y==3)){z=["8:25","8:30","8:42","8:46","9:27","9:31","10:12","10:37","10:41","11:22","11:26","12:07","12:11","12:51","13:11","13:31","13:35","14:15","14:19","14:59","15:03","15:10"]}
+    if (y==4){z=["8:25","8:30","8:42","8:46","9:30","9:34","10:18","10:43","10:47","11:30","11:34","12:17","12:21","12:51","13:11","13:31","13:35","14:15","14:19","14:59","15:03","15:10"]}
+    for (var i in z){
+        k=new Date(z[i]+" "+parseInt(x.getMonth()+1)+"/"+x.getDate()+"/"+x.getFullYear());
+        if (x.getTime()<k.getTime()){
+            return [format(k.getTime()-x.getTime()),format(k.getTime()-x.getTime())]
         }
     }
 }
@@ -55,5 +38,20 @@ function output(){
     document.getElementById("timeleftbox").innerHTML=outputstring()[0];
     document.title="Time left: "+outputstring()[1];
 }
+
+s=document.getElementById("slider");
+t=document.getElementById("slidertext");
+var v=0;
+if (localStorage.getItem("clocktime")){s.value=parseInt(localStorage.getItem("clocktime"))}
+s.oninput = function(){
+    v=parseInt(this.value);
+    if (v%2==0){s="&nbsp&nbsp&nbsp"}else{s=""}
+    if (this.value<0){t.innerHTML="My clock is "+s+(-this.value/2)+" seconds behind the bell"}
+    if (this.value>0){t.innerHTML="My clock is "+s+this.value/2+" seconds ahead of the bell"}
+    if (this.value==0){t.innerHTML="My clock is pretty close to the bell"}
+    localStorage.setItem("clocktime",String(v));
+}
+s.oninput()
+
 output();
 var dostuff=setInterval(output,250)
